286a848409fdd1d16cfa9b64adc22c1cb7f5a26c
Louis <louis.retif@protonmail.com>
2024-07-07 17:26:26 +0200

    Create courseid.dart

---
+++ lib/courseid.dart
@@ -0,0 +1,49 @@
+List<Map> citise1 = const [
+  {
+    //S1
+    'Mathématiques': '309',
+    'Mécanique du point': '450',
+    'Optique géométrique': '451',
+    'Anglais': '542',
+    'LV2': '1014',
+    'Communication': '544',
+    'Collaborer et communiquer': '1014',
+    'Projet scientifique bibliographique': '545'
+  },
+  {
+    //S2
+    'Mathématiques': '381',
+    'Electrostatique': '66',
+    'Magnétostatique': '549',
+    'Anglais': '546',
+    'LV2': '911',
+    'Communication (Français, orthographe)': '547',
+    'Projet de physique': '215',
+    'Etudes et réalisations pluritechnologiques': '485'
+  }
+];
+
+List<Map> citise2 = const [
+  {
+    //S3
+    'Mathématiques': '67',
+    'Induction électromagnétique': '453',
+    'Ondes et propagation': '384',
+    'Anglais': '36',
+    'LV2': '912',
+    'Entreprise et communication': '550',
+    'Projet TIPE': '35'
+  },
+  {
+    //S4
+    'Mathématiques': '465',
+    'Interferences et diffraction optiques': '502',
+    'Probabilités et statistiques': '470',
+    'Anglais': '553',
+    'Connaissance des entreprises': '552',
+    'Principe de la thermodynamique': '24'
+  }
+];
+List<Map> fise1 = const [{}, {}];
+List<Map> fise2 = const [{}, {}];
+List<Map> fise3 = const [{}, {}];
